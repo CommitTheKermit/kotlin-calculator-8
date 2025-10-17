32b83034b81203549dbabea5166131e49753204b
friendshipHyun <commit3921@gmail.com>
2025-10-17 09:12:48 +0900

    feat: Delimiter 클래스 생성

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,10 +2,15 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console
 
+class Delimiter(val input: String) {
+
+}
+
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
 
+    val delimiter = Delimiter(input)
     val result = 0
     println("결과 : $result")
 }
