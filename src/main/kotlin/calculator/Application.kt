8eb5de770c00493f8587440a9dab2e87183270e8
friendshipHyun <commit3921@gmail.com>
2025-10-17 09:12:33 +0900

    feat: 계산 결과 출력

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,4 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
     val input = Console.readLine()
+
+    val result = 0
+    println("결과 : $result")
 }
